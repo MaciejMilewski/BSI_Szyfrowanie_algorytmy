16ed2cfc34f800cf0ba820ea44a51405b9308c8c
michal <micdeg96@gmail.com>
2020-12-15 16:04:43 +0100

    Szyfrowanie AES - zmiany

--- src/aes.py
+++ src/aes.py
@@ -6,38 +6,40 @@ Autor: Michał Degowski
 """
 
 from Crypto.Cipher import AES
+from utils.pad import pad, unpad
 import base64
 # functions for encoding binary data to printable ASCII characters and decoding such encodings back to binary data
 
-def aes_encode(message, key, iv):
+def aes_encode(message, key):
+    """ Encrypting message using AES in CBC Mode """
     key = bytes(key, encoding='utf8')
+    iv = "mm88!@#$%^dsmdms"
     iv = bytes(iv, encoding='utf8')
-    # print(iv) = "mm88!@#$%^dsmdms"
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    pad = lambda s: s + (16 - len(s) % 16) * '\0'
     AES_code = bytes(pad(message), encoding='utf8')
     code = cipher.encrypt(AES_code)
     encrypted_text = str((base64.encodebytes(code)).decode())
     return encrypted_text
 
 
-def aes_decode(message, key, iv):
+def aes_decode(message, key):
+    """ Decrypting message and removing padding from message """
     key = bytes(key, encoding='utf8')
+    iv = "mm88!@#$%^dsmdms"
     iv = bytes(iv, encoding='utf8')
-    # print(iv) = "mm88!@#$%^dsmdms"
     cipher = AES.new(key, AES.MODE_CBC, iv)
     message = base64.b64decode(message.encode())
     decrypted_text = cipher.decrypt(message).decode()
     decrypted_code = decrypted_text.rstrip('\0')
-    return decrypted_code
+    return unpad(decrypted_code)
 
 def main():
+    """ Crypt&Decrypt message using AES """
     message = input("Podaj wiadomość: \n")
     secret_key = input("Podaj klucz (musi być 16 znaków!): \n")
-    iv = "mm88!@#$%^dsmdms"
-    encrypted_text = aes_encode(message, secret_key, iv)
+    encrypted_text = aes_encode(message, secret_key)
     print('Zaszyfrowany tekst: ', encrypted_text)
-    print('Odszyfrowany tekst: ', aes_decode(encrypted_text, secret_key, iv))
+    print('Odszyfrowany tekst: ', aes_decode(encrypted_text, secret_key))
 
 
 if __name__ == "__main__":
---
+++ utils/pad.py
@@ -0,0 +1,9 @@
+def pad(s):
+    pad = 16 - len(s) % 16
+    return s + pad * chr(pad)
+
+def unpad(s):
+    s = s
+    offset = ord(s[-1])
+    #ord() function returns an integer representing the Unicode character
+    return s[:-offset]
